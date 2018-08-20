6da579ba9faea1c84cd04c235a1a5372444c5a21
Jhedi Ayala <jhedi@Jhedis-MacBook-Pro.local>
2018-08-20 00:01:49 -0400

    smoothe scroll button

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ $(document).ready(function(){
         $("nav ul li").toggleClass("mobile");
         console.log('clicking')
     });
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 50) {
+            $('#backToTop').fadeIn('slow');
+        } else {
+            $('#backToTop').fadeOut('slow');
+        }
+    });
+    $('#backToTop').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+
 });
 
 
