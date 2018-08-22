f2690b8b36800ac13c8be19a28182a41764d3bd9
Jhedi Ayala <jhedi@Denzels-iPad.localdomain>
2018-08-22 15:07:05 -0400

    first mobile hover pass

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
         }
     });
     $('#backToTop').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 700);
+        $("html, body").animate({ scrollTop: 0 }, 500);
         return false;
     });
 
